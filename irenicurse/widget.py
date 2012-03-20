a053e7f41223816917d10ebfae429cb0c8d24a5d
Laurent Peuch <cortex@worlddomination.be>
2012-03-20 07:50:53 +0100

    [doc] add docstring to bind_to_key

--- irenicurse/widget.py
+++ irenicurse/widget.py
@@ -3,6 +3,11 @@ import urwid
 import factories
 
 def bind_to_key(key):
+    """
+    A decorator to bind the call of a widget's method to a key press event.
+
+    It will not change the method signature.
+    """
     def decorate(func):
         func.__keys__ = func.__keys__ + [key] if hasattr(func, "__keys__") else [key]
         return func
