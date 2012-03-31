a853fe55b26fafe1a386a183ea5938bd213ad61d
Laurent Peuch <cortex@worlddomination.be>
2012-03-31 13:47:33 +0200

    [enh] allow to define a RowTextWidget has walkable

--- irenicurse/widget/row_widget.py
+++ irenicurse/widget/row_widget.py
@@ -2,9 +2,10 @@ import urwid
 
 
 class RowTextWidget(urwid.AttrMap):
-    def __init__(self, data, highligh_colour='reveal focus', prefix="", suffix="", colour=None):
+    def __init__(self, data, highligh_colour='reveal focus', prefix="", suffix="", walkable=True, colour=None):
         self.data = data
         self.highligh_colour = highligh_colour
+        self.walkable = walkable
         self.prefix = prefix
         self.suffix = suffix
         urwid.AttrMap.__init__(self, urwid.Text(self.format_data(self.data)), colour, highligh_colour)
