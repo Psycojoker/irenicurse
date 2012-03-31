1660a9142785755585309a9206c1bbe1a574c11a
Laurent Peuch <cortex@worlddomination.be>
2012-03-31 13:47:01 +0200

    [enh] allow to specify a colour at the creation of a RowTextWidget

--- irenicurse/widget/row_widget.py
+++ irenicurse/widget/row_widget.py
@@ -2,12 +2,12 @@ import urwid
 
 
 class RowTextWidget(urwid.AttrMap):
-    def __init__(self, data, highligh_colour='reveal focus', prefix="", suffix=""):
+    def __init__(self, data, highligh_colour='reveal focus', prefix="", suffix="", colour=None):
         self.data = data
         self.highligh_colour = highligh_colour
         self.prefix = prefix
         self.suffix = suffix
-        urwid.AttrMap.__init__(self, urwid.Text(self.format_data(self.data)), None, highligh_colour)
+        urwid.AttrMap.__init__(self, urwid.Text(self.format_data(self.data)), colour, highligh_colour)
 
     def format_data(self, text):
         return self.prefix + text + self.suffix if self.prefix or self.suffix else text
